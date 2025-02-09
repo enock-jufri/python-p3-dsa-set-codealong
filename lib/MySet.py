aacdb142f36d5d700bb55e21e8316de3ed362c2b
enock-jufri <enockjufri@gmail.com>
2025-02-09 09:47:53 +0300

    custom set data structure

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,29 @@
 class MySet:
+    def __init__(self,list=[]):
+        self.dictionary={}
+        for value in list:
+            self.dictionary[value] = True
 
-    pass
+    def __str__(self):
+        return f"MySet: {{{','.join(map(str, self.dictionary))}}}"
+
+    def has(self,value):
+        return value in self.dictionary
+
+    def add(self,value):
+        self.dictionary[value] = True
+        return self
+
+    def delete(self,value):
+        self.dictionary.pop(value,None)
+        return self
+
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary.clear()
+        return self
+
+st=MySet([1,2,3,4,4])
+print(st)
